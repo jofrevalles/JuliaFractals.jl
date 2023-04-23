04f7678a9c4ff52a9d48e8bad6f0482e1d289d72
Jofre Vallès <jofrevalles99@gmail.com>
2023-04-23 11:10:14 +0200

    Fix examples

--- examples/example1.jl
+++ examples/example1.jl
@@ -4,12 +4,16 @@ CairoMakie.activate!(type = "png")
 
 c = -0.8 + 0.156im
 
-w = 1000
-h = 800
-style = :smooth
+w = 2000
+h = 2000
+style = :continuous_potential
 max_iter = 100
-img = generate_set(style, w, h, (-2.0, 2.0), (-2.0, 2.0), max_iter; c=c)
 
-cmap = :algae
+cmap = :deep
+
+julia_fractal = julia(c, max_iter)
+julia_fractal = mandelbrot(max_iter)
+img = generate_image(julia_fractal, style, w, h, (-2.0, 2.0), (-2.0, 2.0), max_iter)
+
+# f = visualize(img, :makie, cmap=cmap)
 f = visualize(real.(log10.(Complex.(img))), :makie; cmap=cmap)
-save("real.log10.Complex._julia_set_c($c)_cmap($cmap)_style($style)_maxit($max_it).png", f)
--- examples/example2.jl
+++ examples/example2.jl
@@ -2,19 +2,21 @@ using JuliaFractals
 using CairoMakie
 CairoMakie.activate!(type = "png")
 
-function fractal_cloud_iteration(a::ComplexF64, b::ComplexF64, c::ComplexF64)
-    return (z::ComplexF64) -> exp(c * (z^3 + a * z^2 + b * z))
-end
+fractal_func(z) = z^3 - 0.1 + 0.65im
+
 
 a = complex(-0.5, 0.2)
 b = complex(1.3, -0.1)
-c = complex(-1.8, 0.156)
+c = complex(-0.45, 0.156)
 
 w = 1000
-h = 800
-style = :custom_julia
+h = 1000
 max_iter = 100
-img = generate_set(style, w, h, (-1.0, 1.0), (-1.0, 1.0), max_iter; c = c,f=fractal_cloud_iteration(a, b, c))
-
 cmap = :deep
-f = visualize(real.(log10.(Complex.(img))), :makie; cmap=cmap)
+style = :angle_color
+fractal = custom_julia(fractal_func, max_iter)
+
+img = generate_image(fractal, style, w, h, (-2.0, 2.0), (-2.0, 2.0), max_iter)
+
+f = visualize(img, :makie, cmap=cmap)
+# f = visualize(real.(log10.(Complex.(img))), :makie; cmap=cmap)
